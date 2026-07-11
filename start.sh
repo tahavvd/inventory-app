328f1a3b9e127ef7747ad6cf7dd1ed21c85b02fa
Taha Touil <tahatouiloffi@gmail.com>
2026-07-11 03:48:33 +0100

    Ensure sites-enabled directory exists before writing nginx config

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 set -e
 
 export PORT="${PORT:-80}"
+mkdir -p /etc/nginx/sites-enabled
 envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/sites-enabled/default
 
 php-fpm -D
